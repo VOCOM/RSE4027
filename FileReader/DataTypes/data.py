100583f36047dd4f291b7c1de2e7e523ef124949
Muhd Syamim <Syazam33@gmail.com>
2023-10-24 15:55:15 +0800

    Migrated to pandas

--- FileReader/DataTypes/data.py
+++ FileReader/DataTypes/data.py
@@ -16,9 +16,28 @@ class DATA:
         for dataLine in self.data:
             if self.namePos > -1:
                 self.CleanName(dataLine)
+                self.CleanAge(dataLine)
+                self.CleanNumSiblingSpouse(dataLine)
+                self.CleanNumParentChild(dataLine)
+                self.CleanSurvived(dataLine)
 
     def CleanName(self, dataLine):
         dataLine[self.namePos] = dataLine[self.namePos].replace("\"", "")
+
+    def CleanAge(self, dataLine):
+        dataLine[7] = int(dataLine[7].split('.')[0])
+
+    def CleanNumSiblingSpouse(self, dataLine):
+        dataLine[9] = int(dataLine[9])
+
+    def CleanNumParentChild(self, dataLine):
+        dataLine[10] = int(dataLine[10])
+
+    def CleanSurvived(self, dataLine):
+        if dataLine[11].upper() == "YES":
+            dataLine[11] = 1
+        elif dataLine[11].upper() == "NO":
+            dataLine[11] = 0
 
     def GenerateDictionary(self):
         headerIndex = 0
--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ import os
 from FileReader import Reader
 from EDA import eda
 
+import pandas
+from sklearn import linear_model
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -24,6 +27,7 @@ def Operations():
         "2) Print data table",
         "3) Data Distributions",
         "4) Filtered table",
+        "5) Multiple Regression",
         "E) Exit Program"
     ]
 
@@ -53,9 +57,9 @@ def Menu():
         project = input("Select a project: ")
 
         if project == "1":
-            dataPath = "Milestone1"
+            dataPath = "./Milestone1"
         elif project == "2":
-            dataPath = "Milestone2"
+            dataPath = "./Milestone2"
         elif project.capitalize() == "E":
             exit()
         else:
@@ -73,30 +77,49 @@ headerFormat = {
     "Ticket Class" : int
 }
 
-data = Reader.loadData(dataPath + "/train")
+# data = Reader.loadData(dataPath + "/train")
+data = pandas.read_csv(dataPath + "/train/MS_1_Scenario_train.csv")
+
+data["Gender"] = data["Gender"].replace({"female" : 1}, regex=True)
+data["Gender"] = data["Gender"].replace({"male" : 0}, regex=True)
+data["Survived"] = data["Survived"].replace({"Yes" : 1}, regex=True)
+data["Survived"] = data["Survived"].replace({"No" : 0}, regex=True)
+data["NumParentChild"].convert_dtypes(convert_integer=True)
+data["NumSiblingSpouse"].convert_dtypes(convert_integer=True)
+data["Age"].convert_dtypes(convert_integer=True)
+
 func = 0
 while Operations():
     os.system("clear")
     if func == "1":
         pass
     if func == "2":
-        data.PrintData()
+        print(data.to_string(), "\n")
     if func == "3":
-        eda.Info(data)
+        data["Survived"].plot(kind="hist")
     if func == "4":
-        print(data.header)
+        header = list(data.keys())
+        print(header)
         func = input("Choose a category to search for: ")
-        if func in data.dict.keys():
-            distributionTable = eda.Find(data, func)
-            eda.PrintDistribution(distributionTable)
-            
-            xAxis = np.array(list(distributionTable.keys()))
-            yAxis = np.array(list(distributionTable.values()))
-            print(xAxis)
-            print(yAxis)
-            plt.bar(xAxis, yAxis)
-            plt.show()
+        if func in header:
+            print(data[func].to_string())
         else:
             os.system("clear")
-            print("Category not found")
+            print("Category not found\n")
+    if func == "5":
+        # Multiple Regression
+        regr = linear_model.LinearRegression()
+        X = data[["Age", "NumParentChild", "NumSiblingSpouse"]]
+        y = data["Survived"]
+
+        regr.fit(X,y)
+        # 801,$18,2,234360,0,S,"Milling, Mr. Jacob Christian",48,male,0,0,No
+        # 814,$61.9292,1,PC 17485,A20,C,"Duff Gordon, Sir. Cosmo Edmund (""Mr Morgan"")",49,male,1,0,Yes
+        predictedSurvival = regr.predict([[49, 1, 0]])
+        print(predictedSurvival)
+        if predictedSurvival < 0.5:
+            print("Passenger will not survive")
+        else:
+            print("Passenger will survive")
+        pass
 
