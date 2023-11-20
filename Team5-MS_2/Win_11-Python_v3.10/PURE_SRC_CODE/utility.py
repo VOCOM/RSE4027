11217862b651d738a8f633f91a1938fbea42f117
Muhd Syamim <Syazam33@gmail.com>
2023-11-20 14:37:17 +0800

    Update

--- Team5-MS_2/Win_11-Python_v3.10/PURE_SRC_CODE/utility.py
+++ Team5-MS_2/Win_11-Python_v3.10/PURE_SRC_CODE/utility.py
@@ -4,6 +4,9 @@
 #   Created utility library
 ##
 
+# Dataframe Import
+import pandas
+
 # Math Import
 import numpy
 
@@ -18,15 +21,19 @@ def Str2NaN(value):
         value = numpy.nan
     return value
 
-def Metrics(testValue, predictionValue, predictedProbability = 0, isMultiClass = False):
-    trueValue = testValue
+def Metrics(predictionData, predictionValue, config):
+    trueValue = predictionData[config['Parameters']['Prediction Element']].values
     predictedValue = predictionValue
-    average = 'micro'
-    if isMultiClass:
-        auc         = roc_auc_score(trueValue, predictedProbability, multi_class='ovr')
-        precision   = precision_score(trueValue, predictedValue, average=average)
-        recall      = recall_score(trueValue, predictedValue, average=average)
-        f1score     = f1_score(trueValue, predictedValue, average=average)
+
+    predictedData = pandas.DataFrame(data=predictedValue, columns=['Prediction'])
+    predictedData = pandas.get_dummies(predictedData, columns=['Prediction'], drop_first=False, prefix='', prefix_sep='')
+
+    average = None
+    if config['Multi-Class']:
+        auc         = roc_auc_score(trueValue, predictedData, multi_class='ovo')
+        precision   = precision_score(trueValue, predictedValue, average=average, labels=list(config['Classifications'].values()))
+        recall      = recall_score(trueValue, predictedValue, average=average, labels=list(config['Classifications'].values()))
+        f1score     = f1_score(trueValue, predictedValue, average=average, labels=list(config['Classifications'].values()))
     else:
         auc = roc_auc_score(trueValue, predictedValue)
         precision   = precision_score(trueValue, predictedValue)
@@ -50,22 +57,71 @@ def Metrics(testValue, predictionValue, predictedProbability = 0, isMultiClass =
     }
     return metrics
 
-def VisualizeMetrics(metrics):
-    fig, ax = plt.subplots()
-    label = metrics.keys()
-    count = metrics.values()
-    bar = ax.bar(label, count)
-    ax.bar_label(bar, fmt='{:.2f}')
-    plt.show()
+def VisualizeMetrics(metrics, config):
+    metricLabel = []
+    metricCount = []
+    if config['Multi-Class']:
+        i = 0
+        while i < len(metrics['Precision']):
+            metricLabel.append('Precision')
+            metricLabel.append('Recall')
+            metricLabel.append('F1 Score')
+            metricCount.append(metrics['Precision'][i])
+            metricCount.append(metrics['Recall'][i])
+            metricCount.append(metrics['F1'][i])
+            print("Precision of", list(config['Classifications'].keys())[i], ": {:.5f}".format(metrics['Precision'][i]))
+            print("Recall of   ", list(config['Classifications'].keys())[i], ": {:.5f}".format(metrics['Recall'][i]))
+            print("F1 Score of ", list(config['Classifications'].keys())[i], ": {:.5f}".format(metrics['F1'][i]))
+            print()
+            i += 1
+    else:
+        metricLabel.append('Precision')
+        metricLabel.append('Recall')
+        metricLabel.append('F1 Score')
+        metricCount.append(metrics['Precision'])
+        metricCount.append(metrics['Recall'])
+        metricCount.append(metrics['F1'])
+        print("Precision: {:.5f}".format(metrics['Precision']))
+        print("Recall:    {:.5f}".format(metrics['Recall']))
+        print("F1 Score:  {:.5f}".format(metrics['F1']))
 
+    scoreLabel = ['Accuracy']
+    scoreLabel.append('AUC')
+    scoreLabel.append('MCC')
+    scoreLabel.append('MAE')
+    scoreLabel.append('MSE')
+    scoreLabel.append('RMSE')
+    scoreCount = [metrics['CA']]
+    scoreCount.append(metrics['AUC'])
+    scoreCount.append(metrics['MCC'])
+    scoreCount.append(metrics['MAE'])
+    scoreCount.append(metrics['MSE'])
+    scoreCount.append(metrics['RMSE'])
     print("Accuracy:  {:.5f}".format(metrics['CA']))
-    print("Precision: {:.5f}".format(metrics['Precision']))
-    print("Recall:    {:.5f}".format(metrics['Recall']))
-    print("F1 Score:  {:.5f}".format(metrics['F1']))
-    print("AUC:       {:.5f}".format(metrics['AUC']))
-    print("MCC:       {:.5f}".format(metrics['MCC']))
-    print("MAE:       {:.5f}".format(metrics['MAE']))
-    print("MSE:       {:.5f}".format(metrics['MSE']))
-    print("RMSE:      {:.5f}".format(metrics['RMSE']))
+    print("AUC:  {:.5f}".format(metrics['AUC']))
+    print("MCC:  {:.5f}".format(metrics['MCC']))
+    print("MAE:  {:.5f}".format(metrics['MAE']))
+    print("MSE:  {:.5f}".format(metrics['MSE']))
+    print("RMSE: {:.5f}".format(metrics['RMSE']))
     print()
+    
+    scoreFig, scoreAx = plt.subplots()
+    scoreBar = scoreAx.bar(scoreLabel, scoreCount)
+    scoreAx.bar_label(scoreBar, fmt='{:.2f}')
+
+    if config['Multi-Class']:
+        metricFig, metricAx = plt.subplots(1,len(config['Classifications']), sharey=True)
+        i = 0
+        j = 0
+        while j < len(config['Classifications']):
+            metricBar = metricAx[j].bar(metricLabel[i:i+2], metricCount[i:i+2])
+            metricAx[j].set_title(list(config['Classifications'].keys())[j])
+            metricAx[j].bar_label(metricBar, fmt='{:.2f}')
+            i += 3
+            j += 1
+    else:
+        metricFig, metricAx = plt.subplots()
+        metricBar = metricAx.bar(metricLabel, metricCount)
+        metricAx.bar_label(metricBar, fmt='{:.2f}')
+    plt.show()
     
